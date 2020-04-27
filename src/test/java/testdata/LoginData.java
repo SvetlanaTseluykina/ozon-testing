8bb992ea324f1d46c9878751c5e67c666d1af57c
SvetlanaTseluykina <svetlana.celuykina@mail.ru>
2020-04-27 17:24:44 +0400

    changed email

--- src/test/java/testdata/LoginData.java
+++ src/test/java/testdata/LoginData.java
@@ -1,7 +1,7 @@
 package testdata;
 
 public class LoginData {
-    private static final String CORRECT_EMAIL = "svetlana.celuykina@mail.ru";
+    private static final String CORRECT_EMAIL = "correctEmail@mail.ru";
 
     public static String getCorrectEmail() {
         return CORRECT_EMAIL;
